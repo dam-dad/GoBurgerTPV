bf72cf2b1899fd4874df3ed9dcd070ba53df0d3c
michael trujillo padilla <michaeljtp1@gmail.com>
2021-02-02 18:10:38 +0000

    ajustes menores

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -24,6 +24,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
@@ -32,79 +33,78 @@ import models.MainModel;
 public class MainController implements Initializable {
 
 	// Controllers
-	TPVController tpvController=new TPVController();
-	
+	TPVController tpvController = new TPVController();
+
 	// Model
 //	private ObjectProperty<MainModel> model = new SimpleObjectProperty<>();
-	MainModel model=new MainModel();
-	
+	MainModel model = new MainModel();
+
 	// View
 	@FXML
-    private GridPane view;
+	private GridPane view;
 
-    @FXML
-    private Label usuarioLabel;
+	@FXML
+	private Label usuarioLabel;
 
-    @FXML
-    private Label contraseñaLabel;
+	@FXML
+	private Label contraseñaLabel;
 
-    @FXML
-    private TextField usuarioTf;
+	@FXML
+	private TextField usuarioTf;
 
-    @FXML
-    private TextField contraseñaTf;
+	@FXML
+	private PasswordField contraseñaTf;
 
-    @FXML
-    private Button accederBt;
+	@FXML
+	private Button accederBt;
 
-    @FXML
-    private Button cancelarBt;
+	@FXML
+	private Button cancelarBt;
 
-    @FXML
-    void onActionAcceder(ActionEvent event) throws IOException {
+	@FXML
+	void onActionAcceder(ActionEvent event) throws IOException {
 
-    	String encryptedPass= DigestUtils.md2Hex(model.getContraseña()).toUpperCase();
-    	List<Users> usuarios=Funciones.getUsers();
-    	
-    	for(Users user: usuarios) {
-    		if(user.getUsuario().equals(model.getUsuario())&&user.getPassword().equals(encryptedPass)) {
-    			
-    			if (user.isAdministrador())
-    				model.setAdministrador(true);
-    			App.getPrimaryStage().setScene(new Scene(tpvController.getView()));
-    			
-    	}
-    		
-    	
-    }
-	
+		String encryptedPass = DigestUtils.md2Hex(model.getContraseña()).toUpperCase();
+		List<Users> usuarios = Funciones.getUsers();
+		boolean datosCorrectos = false;
+		model.setUsuario(model.getUsuario().trim());
+		for (Users user : usuarios) {
+			if (user.getUsuario().equals(model.getUsuario()) && user.getPassword().equals(encryptedPass)) {
+				datosCorrectos = true;
+				if (user.isAdministrador())
+					model.setAdministrador(true);
+				App.getPrimaryStage().setScene(new Scene(tpvController.getView()));
+
+			}
+
+		}
+		if(!datosCorrectos) {
+			model.setUsuario("");
+			model.setContraseña("");
+		}
+
 	}
 
-    @FXML
-    void onActionCancelar(ActionEvent event) {
+	@FXML
+	void onActionCancelar(ActionEvent event) {
 
-    }
-    
-    
+	}
+
 	public MainController() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/VistaInicioSesion.fxml"));
 		loader.setController(this);
 		loader.load();
 	}
-    
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 //		model.addListener((o, ov, nv) -> onInicioSesionChanged(o, ov, nv));
 //		
 //		model.set(new MainModel());
-		
+
 		model.usuarioProperty().bindBidirectional(usuarioTf.textProperty());
 		model.contraseñaProperty().bindBidirectional(contraseñaTf.textProperty());
 
-		
-		
-		
 	}
 
 //	
@@ -112,7 +112,6 @@ public class MainController implements Initializable {
 //
 //	}
 
-	
 	public GridPane getView() {
 		return view;
 	}
